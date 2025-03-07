2517f84b1a801a4c9b3feea3e8675ae5554f34b1
YashuCV <yashumanwanth@gmail.com>
2025-03-07 13:12:13 -0600

    Update HomeView.swift
    After user logging in, we are redirected to the home page, where we have two options: start a new match or view the results of completed matches.

--- CricketScoreCard/Views/Home/HomeView.swift
+++ CricketScoreCard/Views/Home/HomeView.swift
@@ -1,1 +1,51 @@
 // Home Page
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            // Heading
+            Text("Cricket Scorecard")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.top, 50)
+            
+            Text("Home Page")
+                .font(.title2)
+                .padding(.bottom, 30)
+            
+            // Start Match Button
+            Button(action: {
+                // Action for starting a match (to be implemented)
+            }) {
+                Text("Start Match")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(5)
+            }
+            .padding(.horizontal, 20)
+            
+            // View Previous Matches Button
+            Button(action: {
+                // Action for viewing previous matches (to be implemented)
+            }) {
+                Text("View Previous Matches")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(5)
+            }
+            .padding(.horizontal, 20)
+            
+            Spacer()
+        }
+        .navigationBarTitle("Home", displayMode: .inline) // Show title in navigation bar
+    }
+}
+
+#Preview {
+    HomeView()
+}
