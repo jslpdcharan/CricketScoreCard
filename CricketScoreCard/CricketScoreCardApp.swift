c48c27ffd3f4c6104719530a864974dff4277158
jslpdcharan <59231349+jslpdcharan@users.noreply.github.com>
2025-03-07 11:32:55 -0600

    Initial Commit

---
+++ CricketScoreCard/CricketScoreCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricketScoreCardApp.swift
+//  CricketScoreCard
+//
+//  Created by Satyanarayana on 07/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CricketScoreCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CricketScoreCardTests/CricketScoreCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  CricketScoreCardTests.swift
+//  CricketScoreCardTests
+//
+//  Created by Satyanarayana on 07/03/25.
+//
+
+import Testing
+@testable import CricketScoreCard
+
+struct CricketScoreCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
