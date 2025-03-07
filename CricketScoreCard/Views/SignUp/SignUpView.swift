1b4e978c3b3b0064b71ed7a82cda7d81556fa016
YashuCV <yashumanwanth@gmail.com>
2025-03-07 13:14:32 -0600

    Update SignUpView.swift
    A new user must sign up before using the app. The sign-up page includes two fields: username and password, allowing users to register. These credentials can then be used to log in to the app.

--- CricketScoreCard/Views/SignUp/SignUpView.swift
+++ CricketScoreCard/Views/SignUp/SignUpView.swift
@@ -1,1 +1,49 @@
-// Sign Up
+import SwiftUI
+
+struct SignupView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var isSignedUp = false
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Sign Up")
+                .font(.largeTitle)
+                .bold()
+            
+            TextField("Username", text: $username)
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(5)
+            
+            SecureField("Password", text: $password)
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(5)
+            
+            Button(action: {
+
+                isSignedUp = true
+            }) {
+                Text("Submit")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.green)
+                    .cornerRadius(5)
+            }
+            
+            NavigationLink(destination: HomeView(), isActive: $isSignedUp) {
+                EmptyView()
+            }
+        }
+        .padding()
+        .navigationBarTitle("Sign Up", displayMode: .inline)
+    }
+}
+
+struct SignupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignupView()
+    }
+}
